cc248ddf32824599100d6f7958893eeeaf8a4e3b
Peoples <67402756+LiterallyLink@users.noreply.github.com>
2025-10-31 23:45:28 -0700

    Init Commit

---
+++ src/client/java/chamncraft/arsenal/CharmncraftsarsenalAndArmoryClient.java
@@ -0,0 +1,10 @@
+package chamncraft.arsenal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CharmncraftsarsenalAndArmoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
